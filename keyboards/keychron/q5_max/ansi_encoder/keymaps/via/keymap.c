52e75ecc6a2e874c5c9f8a63fa232787cf180a67
rootiest <chris@rootiest.com>
2026-04-10 23:29:26 -0400

    feat(q5_max): add shift-backspace → delete key (BSP_DEL)
    Replaces KC_BSPC on the BASE layer with a custom BSP_DEL keycode:
    tap sends Backspace, Shift+tap strips shift and sends Delete.

--- keyboards/keychron/q5_max/ansi_encoder/keymaps/via/keymap.c
+++ keyboards/keychron/q5_max/ansi_encoder/keymaps/via/keymap.c
@@ -38,6 +38,7 @@ enum custom_keycodes {
     LCK_CTL,                  // Lock/unlock KEEB_CTL
     LCK_BASE,                 // Clear all locks and return to BASE
     CAPS_MOD,                 // Tap=ESC, hold=Ctrl, Shift=CapsLock, Alt=CapsWord, GUI=Autocorrect
+    BSP_DEL,                  // Tap=Backspace, Shift+Tap=Delete
 };
 
 // Declare layers early so the HID functions below can reference KEEB_CTL.
@@ -166,7 +167,7 @@ static uint16_t caps_mod_timer           = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ansi_101(
         KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,             KC_DEL,   KC_PSCR,  KC_CALC,  KC_FIND,    KC_MPLY,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,    KC_PMNS,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   BSP_DEL,            KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,    KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         CAPS_MOD, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             TD(TD_HOME_END),  KC_P4,    KC_P5,    KC_P6,      KC_PPLS,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,              KC_P1,    KC_P2,    KC_P3,
@@ -340,6 +341,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     if (target != BASE) {
                         locked_layers = (1UL << target);
                     }
+                }
+            }
+            return false;
+
+        case BSP_DEL:
+            if (record->event.pressed) {
+                uint8_t mods = get_mods();
+                if (mods & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    tap_code(KC_DEL);
+                    set_mods(mods);
+                } else {
+                    tap_code(KC_BSPC);
                 }
             }
             return false;
